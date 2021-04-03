5f85a05775e08f4452d30c6c71c3b633121d2a0d
Jonathan M. Kelly <bitjetkit@gmail.com>
2021-04-03 01:36:40 -0500

    Give opening comments

--- Vector.java
+++ Vector.java
@@ -1,7 +1,10 @@
 package VectorAlgebra;
 /**
- * In the physical sciences and engineering, mathematics is required.
- * This class defines commonly used vector algebra.
+ * @author: Bit Jet Kit
+ * Title: Vector.java
+ * Date: April 3, 2021
+ * Purpose: In the physical sciences and engineering, mathematics is required.
+ *          This class defines commonly used vector algebra.
  */
 public class Vector {
     
